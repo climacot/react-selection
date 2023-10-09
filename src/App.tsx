dbcf688673b1cac414642188fe7e5293f387eedc
climacot <fermt111@gmail.com>
2023-10-09 15:59:51 -0500

    init vite app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#FFFFFFFF",
+        height: "400px",
+        width: "400px",
+      }}
+    />
+  );
+}
+
+export default App;
